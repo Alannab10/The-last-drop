11118c550d7cffdb360ae4c9652c7486f8f8db4e
fer1501 <aleja.jimenezz13@gmail.com>
2025-04-02 15:01:36 -0500

    music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance{get; private set;}
+    [SerializeField] AudioSource soundEffects;
+    [SerializeField] AudioSource music;
+     void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void PlaySFX (AudioClip sound)
+    {
+        soundEffects.PlayOneShot(sound);
+    }
+    public void PlayMusic (AudioClip musicSong)
+    {
+        music.Stop();
+        music.clip = musicSong;
+        music.Play();
+    }
+}
