6518b06ac936bfb0b7c038b79ebfae768eb12cab
Alannab10 <michbejarano421@gmail.com>
2025-04-09 13:31:29 -0500

    Animaciones

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
 
     float horizontalValue;
-    [SerializeField]bool jumpActivated = false;
+    [SerializeField] bool jumpActivated = false;
 
     [SerializeField] float speed;
     [SerializeField] float forceJump;
@@ -20,12 +21,14 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
         horizontalValue = Input.GetAxis("Horizontal");
+        anim.SetBool("InMovement", horizontalValue != 0 ? true : false);
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             jumpActivated = true;
         }
@@ -33,29 +36,34 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rigid.velocity = new Vector2(horizontalValue * speed, rigid.velocity.y);
+        anim.SetFloat("YVelocity", rigid.velocity.y);
 
-        collisions = Physics2D.OverlapCircleAll(detectionPoint.position,detectionSize, detectionLayer);
+        collisions = Physics2D.OverlapCircleAll(detectionPoint.position, detectionSize, detectionLayer);
 
-        if(collisions.Length > 0 )
+        if (collisions.Length > 0)
         {
             isGrounded = true;
         }
 
-        if(collisions.Length == 1 && collisions[0].gameObject == gameObject)
+        anim.SetBool("IsGrounded", isGrounded);
+
+
+        if (collisions.Length == 1 && collisions[0].gameObject == gameObject)
         {
             isGrounded = false;
         }
 
-        if(jumpActivated == true)
+        if (jumpActivated == true)
         {
             rigid.AddForce(Vector2.up * forceJump, ForceMode2D.Impulse);
+            anim.SetTrigger("Jump");
             jumpActivated = false;
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere (detectionPoint.position , detectionSize);
+        Gizmos.DrawWireSphere(detectionPoint.position, detectionSize);
     }
 
 }
